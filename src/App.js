da5a3f4c9e4668a70d6a61405b0541574dd5afe3
kuma <kumaleta2021@gmail.com>
2023-11-10 19:19:13 +0300

    commit all changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+  const steps=[{advice:"learn react",step:1},{advice:"master react library",step:2},{advice:'earn money through freelance',step:3}]
+  const [stepCount,setStepCount]=useState(0)
+  // const [count,setCount]=useState(steps[0])
+function handlePrevious(){
+  if (stepCount < 1 ) return;
+  setStepCount((stepCount) => stepCount - 1);
+}
+function handleNext(){
+ if(stepCount>=2)
+ return
+ setStepCount((stepCount)=>stepCount+1)
+ 
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="steps">
+        <h1 className={stepCount === 0 ? "green" : ""}>step :1</h1>
+        <h1 className={stepCount === 1? "green" : ""}>step :2</h1>
+        <h1 className={stepCount === 2 ? "green" : ""}>step :3</h1>
+      </div>
+      <div>
+        <p style={{}} className="adviceStep">
+          step:{stepCount + 1} {steps[stepCount].advice}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="handleSwitch">
+        <button className="btn" onClick={() => handlePrevious()}>
+          previous
+        </button>
+        <button className="btn" onClick={() => handleNext()}>
+          next
+        </button>
+      </div>
     </div>
   );
 }
